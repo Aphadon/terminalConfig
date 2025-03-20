834c3dc10874165fc4c81fb0cf8e395137c29800
Bjørn Kristian Johnsen <bksjohnsen@gmail.com>
2025-03-20 07:54:59 +0100

    remap for sudo-write added

--- .config/nvim/lua/bkshjohnsen/remap.lua
+++ .config/nvim/lua/bkshjohnsen/remap.lua
@@ -54,3 +54,5 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("c", "w!!", "w !sudo tee > /dev/null %")
