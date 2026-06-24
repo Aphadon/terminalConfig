01c37669fe50cf8fa63a69c7d4ee6c2236007f00
Bjørn Kristian Johnsen <bksjohnsen@gmail.com>
2026-06-24 13:31:27 +0200

    rename var remap added

--- nvim/.config/nvim/lua/aphadon/remap.lua
+++ nvim/.config/nvim/lua/aphadon/remap.lua
@@ -33,6 +33,7 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<C-t>", "<cmd>tabnew<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {desc = "Rename variable" })
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
